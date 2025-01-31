887e0ba68b2e78dcfdc1cf03a4402d6f43e0b004
Carlos Ramos <166812554+void-022@users.noreply.github.com>
2025-01-31 22:26:44 +0800

    Setup project

---
+++ src/main/java/com/github/void022/PhpBanknotesApiApplication.java
@@ -0,0 +1,14 @@
+package com.github.void022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhpBanknotesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhpBanknotesApiApplication.class, args);
+		System.out.print("Hallo world!");
+	}
+
+}
---
+++ src/test/java/com/github/void022/PhpBanknotesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.void022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhpBanknotesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
